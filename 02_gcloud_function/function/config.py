0390894e300932ecaec76f68ce5158d0cd8837c9
Gregory Slater <gslater@edf.org>
2022-01-26 17:30:47 +0000

    add in step to use file.cio

--- 02_gcloud_function/function/config.py
+++ 02_gcloud_function/function/config.py
@@ -3,4 +3,5 @@ target_files = ['rsv', 'rch', 'sed', 'vel', 'hru']
 unique_locations = {'rsv' : 4,
                     'rch' : 332,
                     'sed' : 332,
-                    'vel' : 1}
+                    'vel' : 1,
+                    'hru' : 2169}
--- 02_gcloud_function/function/swat_format.py
+++ 02_gcloud_function/function/swat_format.py
@@ -2,24 +2,24 @@
 import config as cfg
 import pandas as pd
 
-def format_rsv(df, model_period):
+def format_rsv(df, model_period, cio_dict):
 
     if model_period == 'monthly':
             
-            try:
-                # pull min and max year values from MON field (which contains days, months & years)
-                year_start = int(min(df[df['MON'] > 100].MON))
-                year_end = int(max(df[df['MON'] > 100].MON))
-            
-            except:
-                print("Expecting annual averages in monthly file but none found. Is model time interval defined correctly?")
-                raise
-                    
-            # remove annual summaries throughout file, i.e. where MON is not a month or day value
-            df = df[df['MON'] < 1000]
+        try:
+            # pull min and max year values from MON field (which contains days, months & years)
+            year_start = int(min(df[df['MON'] > 100].MON))
+            year_end = int(max(df[df['MON'] > 100].MON))
         
-            # create array of year values to match date sequence in monthly file
-            df['year'] = [i for i in range(year_start, year_end + 1) for k in range(0, (cfg.unique_locations['rsv'] * 12))]
+        except:
+            print("Expecting annual averages in monthly file but none found. Is model time interval defined correctly?")
+            raise
+                
+        # remove annual summaries throughout file, i.e. where MON is not a month or day value
+        df = df[df['MON'] < 1000]
+    
+        # create array of year values to match date sequence in monthly file
+        df['year'] = [i for i in range(year_start, year_end + 1) for k in range(0, (cfg.unique_locations['rsv'] * 12))]
 
     if model_period == 'daily': 
         
@@ -29,80 +29,123 @@ def format_rsv(df, model_period):
     return df
 
 
-def format_rch(df, model_period):
+def format_rch(df, model_period, cio_dict):
 
     if model_period == 'monthly':
             
-            try:
-                # pull min and max year values from MON field (which contains days, months & years)
-                year_start = int(min(df[df['MON'] > 100].MON))
-                year_end = int(max(df[df['MON'] > 100].MON))
-            
-            except:
-                print("Expecting annual averages in monthly file but none found. Is model time interval defined correctly?")
-                raise
-                    
-            # remove annual summaries throughout file, i.e. where MON is not a month or day value
-            df = df[df['MON'] < 1000]
-
-            # cut off total average summary from bottom of file
-            df = df.iloc[0:len(df)-332, :]
+        try:
+            # pull min and max year values from MON field (which contains days, months & years)
+            year_start = int(min(df[df['MON'] > 100].MON))
+            year_end = int(max(df[df['MON'] > 100].MON))
         
-            # create array of year values to match date sequence in monthly file
-            df['year'] = [i for i in range(year_start, year_end + 1) for k in range(0, (cfg.unique_locations['rch'] * 12))]
-            # set empty value for day, which doesn't exist in monthly output, but does in daily
-            df['day'] = 0
+        except:
+            print("Expecting annual averages in monthly file but none found. Is model time interval defined correctly?")
+            raise
+                
+        # remove annual summaries throughout file, i.e. where MON is not a month or day value
+        df = df[df['MON'] < 1000]
 
-    if model_period == 'daily': 
-        
-        # df = df.drop(['day'], axis=1)
-        pass
-
-    return df
-
-
-def format_sed(df, model_period):
-
-    if model_period == 'monthly':
-            
-            try:
-                # pull min and max year values from MON field (which contains days, months & years)
-                year_start = int(min(df[df['MON'] > 100].MON))
-                year_end = int(max(df[df['MON'] > 100].MON))
-            
-            except:
-                print("Expecting annual averages in monthly file but none found. Is model time interval defined correctly?")
-                raise
-                    
-            # remove annual summaries throughout file, i.e. where MON is not a month or day value
-            df = df[df['MON'] < 1000]
-
-            # cut off total average summary from bottom of file
-            df = df.iloc[0:len(df)-332, :]
-        
-            # create array of year values to match date sequence in monthly file
-            df['year'] = [i for i in range(year_start, year_end + 1) for k in range(0, (cfg.unique_locations['sed'] * 12))]
+        # cut off total average summary from bottom of file
+        df = df.iloc[0:len(df)-332, :]
+    
+        # create array of year values to match date sequence in monthly file
+        df['year'] = [i for i in range(year_start, year_end + 1) for k in range(0, (cfg.unique_locations['rch'] * 12))]
+        # set empty value for day, which doesn't exist in monthly output, but does in daily
+        df['day'] = 0
 
     if model_period == 'daily': 
         
         try:
+            start = '01/01/' + str(cio_dict['year_start'])
+            end = '31/12/' + str(cio_dict['year_start'] + cio_dict['n_years'] - 1)
+            
             # create array of year values to match date sequence in daily file
-            df['year'] = [i for i in pd.date_range(start = '01/01/2003', end = '31/12/2020', freq = 'D').year for k in range(0, (cfg.unique_locations['sed']))]
+            df['year'] = [i for i in pd.date_range(start = start, end = end, freq = 'D').year for k in range(0, (cfg.unique_locations['rch']))]
     
         except:
-            print("Expecting daily averages in monthly file but none found. Is model time interval defined correctly?")
+            print("Length of daily output file is not as expected from the file.cio, please check they're from the same model run")
             raise
 
     return df
 
 
-def format_vel(df, model_period):
+def format_sed(df, model_period, cio_dict):
+
+    if model_period == 'monthly':
+            
+        try:
+            # pull min and max year values from MON field (which contains days, months & years)
+            year_start = int(min(df[df['MON'] > 100].MON))
+            year_end = int(max(df[df['MON'] > 100].MON))
+        
+        except:
+            print("Expecting annual averages in monthly file but none found. Is model time interval defined correctly?")
+            raise
+                
+        # remove annual summaries throughout file, i.e. where MON is not a month or day value
+        df = df[df['MON'] < 1000]
+
+        # cut off total average summary from bottom of file
+        df = df.iloc[0:len(df)-332, :]
+    
+        # create array of year values to match date sequence in monthly file
+        df['year'] = [i for i in range(year_start, year_end + 1) for k in range(0, (cfg.unique_locations['sed'] * 12))]
+
+    if model_period == 'daily': 
+        
+        try:
+            start = '01/01/' + str(cio_dict['year_start'])
+            end = '31/12/' + str(cio_dict['year_start'] + cio_dict['n_years'] - 1)
+            
+            # create array of year values to match date sequence in daily file
+            df['year'] = [i for i in pd.date_range(start = start, end = end, freq = 'D').year for k in range(0, (cfg.unique_locations['sed']))]
+    
+        except:
+            print("Length of daily output file is not as expected from the file.cio, please check they're from the same model run")
+            raise
+
+    return df
+
+
+def format_vel(df, model_period, cio_dict):
 
     df = df.melt(id_vars = ['Day', 'Year'], var_name = "sub", value_name = "velocity")
 
     return df
 
 
-def format_hru(df, model_period):
+def format_hru(df, model_period, cio_dict):
 
+    if model_period == 'monthly':
+            
+        try:
+            # pull min and max year values from MON field (which contains days, months & years)
+            year_start = int(min(df[df['MON'] > 100].MON))
+            year_end = int(max(df[df['MON'] > 100].MON))
+        
+        except:
+            print("Expecting annual averages in monthly file but none found. Is model time interval defined correctly?")
+            raise
+                
+        # remove annual summaries throughout file, i.e. where MON is not a month or day value
+        df = df[df['MON'] < 1000]
+
+        # cut off total average summary from bottom of file
+        df = df.iloc[0:len(df)-332, :]
+    
+        # create array of year values to match date sequence in monthly file
+        df['year'] = [i for i in range(year_start, year_end + 1) for k in range(0, (cfg.unique_locations['hru'] * 12))]
+
+    if model_period == 'daily': 
+        
+        try:
+            start = '01/01/' + str(cio_dict['year_start'])
+            end = '31/12/' + str(cio_dict['year_start'] + cio_dict['n_years'] - 1)
+            
+            # create array of year values to match date sequence in daily file
+            df['year'] = [i for i in pd.date_range(start = start, end = end, freq = 'D').year for k in range(0, (cfg.unique_locations['hru']))]
+    
+        except:
+            print("Length of daily output file is not as expected from the file.cio, please check they're from the same model run")
+            raise
     return df
