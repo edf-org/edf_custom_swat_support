a42a2c64de440f57ae17c3ef0091952e6f0ef884
Gregory Slater <gslater@edf.org>
2022-12-06 14:14:47 +0000

    update facility matching to correct locations

--- 01_geo_facility_locations/01_geocode_and_merge.R
+++ 01_geo_facility_locations/01_geocode_and_merge.R
@@ -12,7 +12,7 @@
 
 ## load up packages
 
-pacman::p_load(tidyverse, lubridate, RColorBrewer, scales, sf, jsonlite, httr)
+pacman::p_load(tidyverse, lubridate, RColorBrewer, scales, sf, jsonlite, httr, geosphere, units)
 
 source("src/edf_theme.R")
 source("src/functions.R")
@@ -22,10 +22,10 @@ theme_set(theme_edf())
 
 # DATA IN ---------------------------------------------------------------
 
-# read in original facilities data and project to NAD83
-fac_orig.sf <- st_read("data/facilities/facilities_v2021Jul28_HUC8pts/facilities_v2021Jul28_HUC8pts.shp",
-                       stringsAsFactors = F) %>% 
-  st_transform(4269) %>% 
+# read in original facilities data
+# this is RJ's final file, after geocoding all facilities using their address info
+# the lcation of facilities after geocoding is in the 'Latitude' and 'Longitude' fields
+fac_orig.df <- read_csv("data/facilities/from RJ/facilities_20220311.csv") %>% 
   rename(REGISTRY_ID = REGISTRY_I,
          LOCATION_ADDRESS = LOCATION_A,
          PRIMARY_NAME = PRIMARY_NA,
@@ -33,13 +33,31 @@ fac_orig.sf <- st_read("data/facilities/facilities_v2021Jul28_HUC8pts/facilities
          FAC_REFERENCE = FAC_REFERE,
          LONGITUDE83 = LONGITUDE8,
          COUNTY_NAME = COUNTY_NAM) %>% 
-  mutate(edf_source = "facilities_v2021Jul28_HUC8pts.shp",
-         latlon_merge_src = "facilities_v2021Jul28_HUC8pts.shp",
+  mutate(fac_source = "facilities_20220311.csv",
+         latlon_src = "facilities_20220311.csv",
          LATITUDE83 = as.double(LATITUDE83),
          LONGITUDE83 = as.double(LONGITUDE83),
          FAC_ACCURACY = as.double(FAC_ACCURACY))
 
-fac_orig.df <- fac_orig.sf %>% st_drop_geometry()
+
+# # read in original facilities data and project to NAD83
+# fac_orig.sf <- st_read("data/facilities/facilities_v2021Jul28_HUC8pts/facilities_v2021Jul28_HUC8pts.shp",
+#                        stringsAsFactors = F) %>% 
+#   st_transform(4269) %>% 
+#   rename(REGISTRY_ID = REGISTRY_I,
+#          LOCATION_ADDRESS = LOCATION_A,
+#          PRIMARY_NAME = PRIMARY_NA,
+#          FAC_ACCURACY = FAC_ACCURA,
+#          FAC_REFERENCE = FAC_REFERE,
+#          LONGITUDE83 = LONGITUDE8,
+#          COUNTY_NAME = COUNTY_NAM) %>% 
+#   mutate(edf_source = "facilities_v2021Jul28_HUC8pts.shp",
+#          latlon_merge_src = "facilities_v2021Jul28_HUC8pts.shp",
+#          LATITUDE83 = as.double(LATITUDE83),
+#          LONGITUDE83 = as.double(LONGITUDE83),
+#          FAC_ACCURACY = as.double(FAC_ACCURACY))
+# 
+# fac_orig.df <- fac_orig.sf %>% st_drop_geometry()
 
 
 fac_new <- read_csv("data/facilities/PROGbyFRS_uniq_11-21-22.csv")
@@ -47,40 +65,61 @@ fac_new <- read_csv("data/facilities/PROGbyFRS_uniq_11-21-22.csv")
 study_area.sf <- st_read("data/study_area_census_dissolve.gpkg") %>% 
   st_transform(4326)
 
-st_crs(fac_orig)
+# SENSE CHECKS ---------------------------------------------------------------
 
-
-# Analyse data ---------------------------------------------------------------
-
+# check what how many records and if REGISTRY_ID is unique
 nrow(fac_orig.df)
-nrow(fac_new)
+nrow(fac_orig.df) == nrow(fac_orig.df %>% distinct(REGISTRY_ID))
 
-nrow(fac_orig.df %>% distinct(REGISTRY_ID))
-nrow(fac_new %>% distinct(REGISTRY_ID))
+nrow(fac_new)
+nrow(fac_new) == nrow(fac_new %>% distinct(REGISTRY_ID))
 
 glimpse(fac_new)
 
+# how many facilites cross over or not between tables
 nrow(inner_join(fac_new, fac_orig.df, by = "REGISTRY_ID"))
 nrow(anti_join(fac_new, fac_orig.df, by = "REGISTRY_ID"))
 
-
+# create table of the actually new facilities
 fac_new_clip <- fac_new %>% 
   anti_join(fac_orig.df, by = "REGISTRY_ID")
 
+# how many of the new facilities are missing location data?
+fac_new_clip %>% 
+  filter(is.na(LATITUDE83)) %>% 
+  nrow()
+
 # write_csv(fac_new_clip, "data/facilities/facilities_new_nodupes.csv")
 
+# create sf of new facilities and transform to WGS84
+fac_new.sf <- fac_new_clip %>% 
+  filter(!is.na(LATITUDE83)) %>% 
+  st_as_sf(coords = c("LONGITUDE83", "LATITUDE83"), crs=4269, remove = F) %>% 
+  st_transform(4326)
 
+# save df with WGS84 location data - we want this later to compare to the geocoding results
+fac_new.df <- fac_new.sf %>%
+  mutate(lon_orig_wgs84 = sf::st_coordinates(.)[,1],
+         lat_orig_wgs84 = sf::st_coordinates(.)[,2]) %>% 
+  st_drop_geometry()
 
-fac_new.sf <- st_as_sf(fac_new_clip, coords = c("LONGITUDE83", "LATITUDE83"), crs=4269)
+# plot of new facilities and study area
+ggplot() +
+  geom_sf(data = study_area.sf, fill = NA) +
+  geom_sf(data = fac_new.sf, col = "red", size = 1) +
+  coord_sf(datum = NA) +
+  theme_minimal()
 
 
 # GEOCODING ---------------------------------------------------------------
 
 
 BASE_URL <- "https://maps.googleapis.com/maps/api/geocode/json?"
-KEY <- "AIzaSyC5U9ireXW6yQJvZclqJVGjBn5PWeEDlgg"
+KEY <- "INSERT_KEY"
 
 # function to hit google geocoding API
+# bounds option doesn't give hard limit but just biases
+# see: https://developers.google.com/maps/documentation/geocoding/
 geoCode <- function(address){
   
   resp <- GET(BASE_URL, 
@@ -100,31 +139,32 @@ geoCode <- function(address){
 
 # test single one
 address <- "6287 GULFWAY DRIVE 77619-4220"
-
 j <- geoCode(address)
 
+# check pulling out key info
 j[[1]]$address_components[[1]]
-
 j[[1]]$formatted_address
-
 j[[1]]$geometry$location$lat
 j[[1]]$geometry$location$lng
 
 
 
 
-# list of facilities to geocode, get rid of NAS in postcode
-to_code <- filter(fac_new_clip, is.na(LATITUDE83)) %>% 
-  mutate(POSTAL_CODE = replace_na(POSTAL_CODE, ""))
+# list of facilities to geocode: all new facilities
+# also, get rid of NAS in postcode and create short version with key 5 digits
+to_code <- fac_new_clip %>% 
+  mutate(POSTAL_CODE = replace_na(POSTAL_CODE, ""),
+         pcode_short = str_sub(POSTAL_CODE, 1, 5))
 
 
 results.df <- NULL
 
 for (i in 1:nrow(to_code)){
+# for (i in 1:2){
   
   # create string address from location + postcode
   address <- paste(to_code$LOCATION_ADDRESS[i],
-                   to_code$POSTAL_CODE[i])
+                   to_code$pcode_short[i])
   
 
   resp <- geoCode(address)
@@ -132,6 +172,7 @@ for (i in 1:nrow(to_code)){
   if (resp$status == "OK"){
     
     # pull out lists of values and fields from the response address_components list
+    # these will be name and value lists of different lengths per response
     addr_values <- resp[["results"]][["address_components"]][[1]][["long_name"]]
     addr_fields <- sapply(resp[["results"]][["address_components"]][[1]][["types"]], function(x) x[1])
     
@@ -146,10 +187,12 @@ for (i in 1:nrow(to_code)){
     
   } else{
     
+    # if error, the only field to save is the result: erro
     fields <- "geocode_result"
     values <- "error"
   }
   
+  # proceed if pulling back fields and values has worked and they're the same length
   if (length(fields) == length(values)){
     # stick in df
     df <- data.frame("REGISTRY_ID" = to_code$REGISTRY_ID[i],
@@ -157,6 +200,7 @@ for (i in 1:nrow(to_code)){
                      "value" = values,
                      stringsAsFactors = FALSE)
   } else {
+    # otherwise create another error
     df <- data.frame("REGISTRY_ID" = to_code$REGISTRY_ID[i],
                      "field" = "geocode_result",
                      "value" = "multiple responses",
@@ -165,30 +209,15 @@ for (i in 1:nrow(to_code)){
   
   # bind to results
   results.df <- bind_rows(results.df, df)
+  
+  # print progress and save every 50 records
+  if (i%%50 == 0){
+    print(paste0("Geocoded ", i, " out of ", nrow(to_code), " records"))
+    write_csv(results.df, "output/geocoding/geocoding_results_narrow.csv")
+  }
 }
 
 # write_csv(results.df, "output/geocoding/geocoding_results_narrow.csv")
-
-# NOTE - results contain an indication of the accuracy of the coding in location_type. Desc. from google:
-# "https://developers.google.com/maps/documentation/geocoding/requests-geocoding#results"
-
-# "ROOFTOP" indicates that the returned result is a precise geocode for which we have 
-#   location information accurate down to street address precision.
-# "RANGE_INTERPOLATED" indicates that the returned result reflects an approximation 
-#   (usually on a road) interpolated between two precise points (such as intersections). 
-#   Interpolated results are generally returned when rooftop geocodes are unavailable for a street address.
-# "GEOMETRIC_CENTER" indicates that the returned result is the geometric center of 
-#   a result such as a polyline (for example, a street) or polygon (region).
-# "APPROXIMATE" indicates that the returned result is approximate.
-
-
-
-# FORMATTING AND MERGING --------------------------------------------------
-
-# inspect errors - just two which have bad input address data
-errors <- filter(results.df, field == "geocode_result" & value != "success") %>% 
-  inner_join(to_code)
-
 
 # pivot results and save
 results_wide <- results.df %>% 
@@ -201,7 +230,52 @@ results_wide <- results.df %>%
 # write_csv(results_wide, "output/geocoding/geocoding_results_wide.csv")
 # results_wide <- read_csv("output/geocoding/geocoding_results_wide.csv")
 
-count(results_wide, location_type) %>% copyExcel()
+
+# GEOCODING RESULTS --------------------------------------------------
+
+# inspect errors - just two which have bad input address data
+errors <- filter(results.df, field == "geocode_result" & value != "success") %>% 
+  inner_join(to_code)
+
+nrow(results_wide)
+
+count(results_wide, location_type) %>% mutate(pct = n/sum(n)) %>% copyExcel()
+count(results_wide, geocode_result)
+
+
+# results_comb <- results_wide %>% 
+#   inner_join(to_code, by = "REGISTRY_ID")
+
+# calculate distance between original points and new geocoded results
+# for 6 facilities this is > 190km
+gc_dist_comparison <- results_wide %>% 
+  inner_join(fac_new.df, by = "REGISTRY_ID") %>% 
+  group_by(REGISTRY_ID) %>% 
+  mutate(distance = set_units(distGeo(c(lng, lat), c(lon_orig_wgs84, lat_orig_wgs84)), m))
+
+gc_dist_comparison %>% filter(as.numeric(distance) <= 10000) %>% nrow() / nrow(gc_dist_comparison)
+
+# plot distribution of point movement (excluding really big jumps)
+fig_dist <- ggplot(gc_dist_comparison %>% 
+         filter(as.numeric(distance) / 1000 < 190), 
+       aes(as.numeric(distance) / 1000)) + 
+  geom_histogram(fill = "steelblue") +
+  scale_x_continuous(breaks = seq(0, 60, 10)) +
+  labs(title = "Facility point movement post-geocoding",
+       subtitle = "Comparison for 486 of the 569 new facilities with location data",
+       x = "Distance (Km)", y = "n facilities")
+
+ggsave("figs/fig - facility point movement post-geocoding.png", fig_dist, units = "cm", width = 15, height = 7)
+
+
+
+# FORMATTING AND MERGING --------------------------------------------------
+
+
+# NEXT STEPS
+# - restrict results to study area
+# - merge to one file, including distance
+
 
 
 # The geocoded results are in WGS84 - so need to be converted to NAD83 before merging with original data
